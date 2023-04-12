01739e9fce4d96abeb0315426a9451a526b5f7e1
Gabrielius-B <g.babicius@rgu.ac.uk>
2023-04-12 17:55:29 +0100

    getting server to work

--- website/index.js
+++ website/index.js
@@ -1,17 +1,25 @@
-const http = require("http");
 const path = require('path');
+const express = require('express');
+const app = express();
 
-const filePath = path.join(__dirname, 'website', req.url === '/' ? 'quotes_and_budgets.html' : req.url);
+const options = {
+    index: "quotes_and_budgets.html"
+  };
 
-const host = 'localhost';
-const port = 8000;
+const dir = path.join(__dirname, '../website');
 
-const requestListener = function(req, res) {
-    res.writeHead(200);
-    res.end("Hello World");
-}
+app.get('/api', function(req, res){
+    res.send("Yes we have an API now")
+});
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
+app.use(express.static(dir, options));
+
+// 404 page
+app.use(function ( req, res, next) {
+    res.send('This page does not exist!')
+});
+
+app.listen(8000, function () {
     console.log('Listening on http://localhost:8000/');
 });
+
